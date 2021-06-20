f8e6b4870061da13502c52a146dbe036528999b5
Frederico Guilherme Kluser de Oliveira <kluserhuu@gmail.com>
2021-06-20 20:09:07 -0300

    Improve Layout

--- index.js
+++ index.js
@@ -4,50 +4,60 @@ let position = 0;
 const data = [
   {
     country: 'Brazil',
-    imageUrl: './cover.jpeg',
+    imageUrl: './brazil.jpeg',
     mapPosition: 'translate(16vw, -16vw)',
     trips: 5,
   },
   {
-    country: 'Africa',
-    imageUrl: './cover.jpeg',
-    mapPosition: 'translate(16vw, -16vw)',
+    country: 'South Africa',
+    imageUrl: './africa.jpeg',
+    mapPosition: 'translate(-3.6vw, -21.7vw)',
     trips: 10,
   },
   {
     country: 'England',
-    imageUrl: './cover.jpeg',
-    mapPosition: 'translate(16vw, -16vw)',
+    imageUrl: './england.jpeg',
+    mapPosition: 'translate(3.4vw, 4.2vw)',
     trips: 18,
   },
 ];
 
-const moveCards = (pos) => {
+const moveCards = (pos, left) => {
   const carrousel = document.getElementById('carrousel');
-  carrousel.setAttribute('class', 'carrousel-container moviment');
+  const map = document.getElementById('world-map');
+  const pointer = document.getElementById('pointer');
+  carrousel.setAttribute('class', `carrousel-container moviment ${left}`);
   carrousel.style.transform = `translateX(-${pos * (329 + 12)}px)`;
+  map.style.transform = `scale(3) ${data[pos].mapPosition}`;
+  pointer.removeAttribute('class');
   Object.values(document.getElementsByClassName('item')).forEach((item, index) => {
     item.setAttribute('class', index === pos ? 'item selected' : 'item');
   });
   setTimeout(() => {
     carrousel.setAttribute('class', 'carrousel-container');
+    map.style.transform = `scale(6) ${data[pos].mapPosition}`;
+    setTimeout(() => {
+      pointer.setAttribute('class', 'active');
+    }, 750);
   }, 250);
 };
 
 const handleGesure = () => {
   let active = false;
+  let left = '';
 
   if (touchendX > touchstartX && position > 0) {
     position--;
     active = true;
+    left = 'left';
   }
-  if (touchendX < touchstartX && position <= data.length) {
+  if (touchendX < touchstartX && position < data.length - 1) {
     position++;
     active = true;
   }
 
   if (active) {
-    moveCards(position);
+    moveCards(position, left);
   }
 };
 
@@ -67,7 +77,7 @@ const renderCards = () => {
 
   carrousel.innerHTML = HTML;
   carrousel.setAttribute('class', 'carrousel-container');
-  carrousel.style.width = `calc(${data.length} * (329px + 12px))`;
+  carrousel.style.width = `calc(${data.length} * (329px + 12px) + 100vw)`;
   carrousel.style.transform = `translateX(-${position * (329 + 12)}px)`;
   document.getElementById('world-map').style.transform = `scale(6) ${data[position].mapPosition}`;
   setTimeout(() => {
